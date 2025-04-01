dd8966cf5d6b0980c64c6c9817d0642e116985e8
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-04-01 23:24:09 +0530

    rte and postform

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,13 @@ import Footer from "./Footer/Footer"
 import {Container} from  "./container/Container"
 import {Logo} from "./Logo"
 import {LogoutBtn} from './Header/LogoutBtn'
+import RTE from "./RTE"
+import Signup from "./SignUp"
+import Login from "./Login"
+import Button from "./Button"
+import Input from "./Input"
+import PostCard from "./PostCard"
+import AuthLayout from "./AuthLayout"
 
-export { Header , Footer,Container, Logo,LogoutBtn }
+
+export { Header , Footer,Container, Logo,LogoutBtn, RTE, Signup, Login, Button, Input, PostCard, AuthLayout}
