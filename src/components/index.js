feb879dd3e63bb373f891790259ce3b4605bc05d
Rishab Sharma <167351217+rishabsharma07@users.noreply.github.com>
2025-04-02 00:25:20 +0530

    pages and routing

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
-import {Container} from  "./container/Container"
-import {Logo} from "./Logo"
-import {LogoutBtn} from './Header/LogoutBtn'
+import Container from  "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from './Header/LogoutBtn'
 import RTE from "./RTE"
 import Signup from "./SignUp"
 import Login from "./Login"
@@ -10,6 +10,7 @@ import Button from "./Button"
 import Input from "./Input"
 import PostCard from "./PostCard"
 import AuthLayout from "./AuthLayout"
+import PostForm from '../components/post-form/PostForm'
+import Select from "./Select"
 
-
-export { Header , Footer,Container, Logo,LogoutBtn, RTE, Signup, Login, Button, Input, PostCard, AuthLayout}
+export { Header , Footer,Container, Logo,LogoutBtn, RTE, Signup, Login, Button, Input, PostCard, AuthLayout, PostForm, Select}
